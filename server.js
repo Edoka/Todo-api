562f88291615f927912516039b0fa113a57aae11
Edoka <edokamezie@gmail.com>
2016-02-22 14:09:18 -0800

    Added underscore modulerefactore API with underscore q

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -19,13 +20,16 @@ app.get('/todos', function(req, res) {
 //GET /todoes/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
 	
-	todos.forEach(function(todo) {
-		if(todoId === todo.id){
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	
+	// var matchedTodo;
+// 	
+	// todos.forEach(function(todo) {
+		// if(todoId === todo.id){
+			// matchedTodo = todo;
+		// }
+	// });
 	
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -56,7 +60,14 @@ app.get('/todos/:id', function(req, res) {
 //POST  /todos
 app.post('/todos', function(req, res) {
 	var body = req.body;
+	body = _.pick(body, 'description', 'completed');
+	
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId++;
+	
 	//todoNextId += 1;
 	todos.push(body);
 	console.log(todos);
