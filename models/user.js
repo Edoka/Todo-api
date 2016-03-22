391e651ee1d777fa56a7a7903d4782f301ba7235
Edoka <edokamezie@gmail.com>
2016-03-22 12:16:34 -0700

    Convert email to lower case

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ module.exports = function(sequelize, DataTypes) {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
+			unique: true,
 			validate: {
 				isEmail: true
 			}
@@ -10,9 +11,17 @@ module.exports = function(sequelize, DataTypes) {
 		password: {
 			type: DataTypes.STRING,
 			allowNull: false,
-			//unique: true,
+			
 			validate: {
 				len: [2, 100]
+			}
+		}
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				}
 			}
 		}
 	});
